e6c96c0b4c7a0e13fba2f5e72d0f0246ebf451c0
tekrajchhetri <tekrajchhetri@gmail.com>
2019-08-05 21:21:52 +0300

    fixed db not found error

--- app.py
+++ app.py
@@ -7,19 +7,13 @@ from resources.user import UserRegister
 from resources.item import Itemlist, Item
 from resources.store import StoreList, Stores
 
-from db import db
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "#123Lterzmne_0983@#"
 api = Api(app)
-db.init_app(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # generates /auth endpoint
 
@@ -31,6 +25,6 @@ api.add_resource(UserRegister, "/register")
 
 
 if __name__ == '__main__':
-    # from db import db
-    # db.init_app(app)
+    from db import db
+    db.init_app(app)
     app.run(debug=True)
